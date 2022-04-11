7386577d80b7cfd9e84d4de51037230aeb216d75
Akhileshsakure <36232800+Akhileshsakure@users.noreply.github.com>
2022-04-12 00:08:20 +0530

    Documentation

--- roll_dice.py
+++ roll_dice.py
@@ -3,45 +3,37 @@ import threading
 import time
 import sys
 
-rollAgain = 'Y'
-Sample = (1, 2, 3, 4, 5, 6)
-threads = []
-
-
-# def dice_result(r):
-#     if r == 1:
-#         print("-------\n|     |\n|  •  |\n|     |\n-------")
-#     elif r == 2:
-#         print("-------\n| •   |\n|     |\n|   • |\n-------")
-#     elif r == 3:
-#         print("-------\n|     |\n|• • •|\n|     |\n-------")
-#     elif r == 4:
-#         print("-------\n| • • |\n|     |\n| • • |\n-------")
-#     elif r == 5:
-#         print("-------\n| • • |\n|  •  |\n| • • |\n-------")
-#     elif r == 6:
-#         print("-------\n|•   •|\n|•   •|\n|•   •|\n-------")
-
-
 def rollDice():
     dicer = random.choice(Sample)
     print("Dice rolled {}".format(dicer))
 
+if __name__ == '__main__':
 
-while rollAgain not in ('N', 'n'):
-    if rollAgain in ('Y', 'y'):
-        no_of_dice = int(input("How many dices do you want to roll : "))
-        start = time.perf_counter()
-        for _ in range(no_of_dice):
-            t = threading.Thread(target=rollDice())
-            t.start()
-            threads.append(t)
+    '''This program rolls n number of dice concurrently by creating n thread instances'''
 
-        for thread in threads:
-            thread.join()
+    rollAgain = 'Y'
+    Sample = (1, 2, 3, 4, 5, 6)
+    threads = []
 
-        end = time.perf_counter()
-        print(f'Finished in {round(end - start, 2)} second(s)')
-        rollAgain = input("Do you want to roll again? (Y or N) : ")
-    else:
-        rollAgain = input(rollAgain + " is invalid input. \nKindly input either 'Y' or 'N' : ")
+
+    while rollAgain not in ('N', 'n'):
+        if rollAgain in ('Y', 'y'):
+            # Enter number of dice to be rolled
+            no_of_dice = int(input("How many dices do you want to roll : "))
+            start = time.perf_counter()
+            # for loop to create multiple thread instances
+            for _ in range(no_of_dice):
+                t = threading.Thread(target=rollDice())
+                # starting thread instance
+                t.start()
+                threads.append(t)
+
+            # for loop to join all child threads with maon thread
+            for thread in threads:
+                thread.join()
+
+            end = time.perf_counter()
+            print(f'Finished in {round(end - start, 2)} second(s)')
+            rollAgain = input("Do you want to roll again? (Y or N) : ")
+        else:
+            rollAgain = input(rollAgain + " is invalid input. \nKindly input either 'Y' or 'N' : ")
